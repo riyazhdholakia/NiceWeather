cb63aedb4d444be079a49cc7bc4c56680bbdb1e1
Nathan Hosselton <nathanhosselton@gmail.com>
2017-08-06 19:25:32 -0400

    Initial Commit

---
+++ NiceWeather/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  NiceWeather
+//
+//  Created by Nathan Hosselton on 8/6/17.
+//  Copyright © 2017 Codebase. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
