94afec9cc73d1afb8e2d83633b47994e13c8946e
Nathan Hosselton <nathanhosselton@gmail.com>
2017-08-06 19:36:30 -0400

    Configure base app for students

---
+++ DarkSky.framework/Headers/DarkSky.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double DarkSkyVersionNumber;
+FOUNDATION_EXPORT const unsigned char DarkSkyVersionString[];
--- NiceWeather/RootViewController.swift
+++ NiceWeather/RootViewController.swift
@@ -12,9 +12,7 @@ class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
 
-
 }
-
